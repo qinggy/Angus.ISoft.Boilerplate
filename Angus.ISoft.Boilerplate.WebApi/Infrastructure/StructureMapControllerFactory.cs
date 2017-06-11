344e86034dc4a1c0e5e22c316efd44a471c7416b
angus qing <angus qing>
2017-06-11 19:30:32 +0800

    解决冲突

--- Angus.ISoft.Boilerplate.WebApi/Infrastructure/StructureMapControllerFactory.cs
+++ Angus.ISoft.Boilerplate.WebApi/Infrastructure/StructureMapControllerFactory.cs
@@ -2,9 +2,9 @@
 *项目名称：Angus Isoft Boilerplate
 *项目描述：继承MVC DefaultControllerFactory 让默认构造Controller实现自定义功能
 *CLR 版本：4.0.30319.42000
-*版 本  号：
-*作      者：Angus
-*修 改  人：
+*版 本 号：
+*作    者：Angus
+*修 改 人：
 *修改说明：
 *创建时间：2016/11/7 14:04:11
 *更新时间：
