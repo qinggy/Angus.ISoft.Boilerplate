a773141268a94e39d607793da0291e0ad86ea0ad
Angus qing <angus_qing168@163.com>
2018-04-05 08:21:17 +0800

    swagger

--- Angus.ISoft.Boilerplate.WebApi/App_Start/SwaggerConfig.cs
+++ Angus.ISoft.Boilerplate.WebApi/App_Start/SwaggerConfig.cs
@@ -32,7 +32,7 @@ namespace Angus.ISoft.Boilerplate.WebApi
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-                        c.SingleApiVersion("v1", "Angus.ISoft.Boilerplate.WebApi");
+                        c.SingleApiVersion("v1", "Boilerplate.WebApi");
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
@@ -61,7 +61,7 @@ namespace Angus.ISoft.Boilerplate.WebApi
                         //c.BasicAuth("basic")
                         //    .Description("Basic HTTP Authentication");
                         //
-                        // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
+						// NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
                         //c.ApiKey("apiKey")
                         //    .Description("API Key Authentication")
                         //    .Name("apiKey")
@@ -101,7 +101,7 @@ namespace Angus.ISoft.Boilerplate.WebApi
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        c.IncludeXmlComments(GetXmlCommentsPath());
+                        //c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -250,11 +250,6 @@ namespace Angus.ISoft.Boilerplate.WebApi
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
-        }
-
-        private static string GetXmlCommentsPath()
-        {
-            return string.Format("{0}/bin/Angus.ISoft.Boilerplate.WebApi.XML", System.AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
