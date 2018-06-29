12e86cdf798aa3d7328033b7b62acf774dabc13d
angus <angus_qing168@163.com>
2018-06-29 21:24:14 +0800

    add log components

---
+++ Angus.ISoft.Boilerplate.Infrastructure/LogComponent/LogTypeEnum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angus.ISoft.Boilerplate.Infrastructure.Log
+{
+    public enum LogTypeEnum
+    {
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        Debug = 1,
+        /// <summary>
+        /// 输出信息
+        /// </summary>
+        Info = 2,
+        /// <summary>
+        /// 警告信息
+        /// </summary>
+        Warn = 3,
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        Error = 4,
+        /// <summary>
+        /// 严重错误
+        /// </summary>
+        Fatal = 5
+    }
+
+    public enum LogWriteDestinationEnum
+    {
+        /// <summary>
+        /// 写入文件
+        /// </summary>
+        WriteToFile = 1,
+        /// <summary>
+        /// 写入数据库
+        /// </summary>
+        WriteToDb = 2
+    }
+}
---
+++ Angus.ISoft.Boilerplate.Infrastructure/LogComponent/LoggerHelper.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angus.ISoft.Boilerplate.Infrastructure.Log
+{
+    public class LoggerHelper
+    {
+        readonly static ILog debug = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        readonly static ILog info = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        readonly static ILog warn = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        readonly static ILog error = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        readonly static ILog fatal = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Error(string errorMsg, Exception ex = null)
+        {
+            if (ex != null) error.Error(errorMsg, ex);
+            else error.Error(errorMsg);
+        }
+
+        public static void Info(string msg)
+        {
+            info.Info(msg);
+        }
+
+        public static void Debug(string msg)
+        {
+            debug.Debug(msg);
+        }
+
+        public static void Warn(string msg)
+        {
+            warn.Warn(msg);
+        }
+
+        public static void Fatal(string msg, Exception ex = null)
+        {
+            if (ex != null) fatal.Fatal(msg, ex);
+            else fatal.Fatal(msg);
+        }
+    }
+}
